cb419cb20404d198e37c7e12477295fb40852e0b
JordanBurnett1210 <jabur1210@gmail.com>
2016-01-19 09:42:29 -0700

    Not a hello world.

---
+++ Debug/src/Controller/SillyAppController.d
@@ -0,0 +1,5 @@
+src/Controller/SillyAppController.o: \
+  ../src/Controller/SillyAppController.cpp \
+  ../src/Controller/SillyAppController.h
+
+../src/Controller/SillyAppController.h:
---
+++ src/Controller/Runner.cpp
@@ -0,0 +1,17 @@
+/*
+ * Runner.cpp
+ *
+ *  Created on: Jan 19, 2016
+ *      Author: jbur0473
+ */
+
+#include "SillyAppController.h"
+
+int main()
+{
+	SillyAppController * baseController = new SillyAppController();
+	baseController->start();
+	return 0;
+}
+
+
